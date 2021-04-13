3f084ce0f71886ef03802824e199733d5ee8474c
André Jaenisch <andre.jaenisch@posteo.de>
2021-04-13 21:56:05 +0200

    Initial setup using esbuild and fastify
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const fastify = require('fastify')({ logger: true })
+
+const PORT = process.env.PORT || 3000
+
+fastify.get('/', async (request, reply) => {
+  const fs = require('fs')
+  const path = require('path')
+  const filePath = path.resolve(path.join(__dirname, 'index.html'))
+  const stream = fs.createReadStream(filePath, 'utf-8')
+  reply.type('text/html').send(stream)
+})
+
+const start = async () => {
+  try {
+    await fastify.listen(PORT)
+  } catch (err) {
+    fastify.log.error(err)
+    process.exit(1)
+  }
+}
+
+start()
