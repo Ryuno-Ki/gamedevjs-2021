5aa7ab9e22490761b6d0b520559e0225b01ec76b
André Jaenisch <andre.jaenisch@posteo.de>
2021-04-16 18:58:57 +0200

    Refactor Level to be rendered from a list of points
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

--- client/index.js
+++ client/index.js
@@ -1,8 +1,39 @@
 'use strict';
 const root = document.getElementById('root')
 const light = document.getElementById('light')
-const path = drawLight(root)
-setTimeout(() => path.style.strokeDashoffset = '0', 500)
+drawLevel(root)
+drawLight(root)
+
+function drawLevel (root) {
+  const points = [
+    [ 15, 90 ],
+    [ 11, 90 ],
+    [ 11, 20 ],
+    [ 21, 10 ],
+    [ 27, 10 ],
+    [ 51, 70 ],
+    [ 55, 70 ],
+    [ 79, 10 ],
+    [ 85, 10 ],
+    [ 95, 20 ],
+    [ 95, 90 ],
+    [ 85, 90 ],
+    [ 85, 24 ],
+    [ 59, 89 ],
+    [ 47, 89 ],
+    [ 21, 24 ],
+    [ 21, 90 ],
+    [ 17, 90 ],
+  ]
+  const d = `M${points[0][0]},${points[0][1]}` + points
+    .slice(1)
+    .map((pair) => `L${pair[0]},${pair[1]}`)
+    .join(' ')
+  const fill = 'none'
+  const stroke = 'black'
+  const path = svg('path', [ 'level' ], { d, fill, stroke, 'stroke-width': 1 })
+  root.appendChild(path)
+}
 
 function drawLight (root) {
   const d = 'M16,91 v-74 h13 v26 h10 v25 h11 v20'
@@ -16,7 +47,7 @@ function drawLight (root) {
   path.getBoundingClientRect()
   path.style.transition = 'stroke-dashoffset 1s linear'
   root.appendChild(path)
-  return path
+  setTimeout(() => path.style.strokeDashoffset = '0', 500)
 }
 
 function svg (tag, classes = [], attrs = {}) {
