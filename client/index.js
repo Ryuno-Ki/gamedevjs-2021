2c4d7a5f47bb8875ea985317bd17bb9ab2ca8016
André Jaenisch <andre.jaenisch@posteo.de>
2021-04-19 22:04:29 +0200

    Render current level into FavIcon. Because we can have SVG there :)
    Signed-off-by: André Jaenisch <andre.jaenisch@posteo.de>

--- client/index.js
+++ client/index.js
@@ -6,5 +6,12 @@ import level from './levels/level0.json'
 const root = document.getElementById('root')
 const light = document.getElementById('light')
 drawLevel(root, level)
-drawLight(root, level, [ 90, 91 ], [ 0, -1 ])
+drawLight(root, level, [ 50, 91 ], [ 0, -1 ])
+updateFavicon(root)
 
+function updateFavicon (svg) {
+  const icon = document.querySelector('link[rel="icon"]')
+  const serializer = new XMLSerializer()
+  const str = serializer.serializeToString(svg)
+  icon.href = 'data:image/svg+xml;utf8,' + str
+}
